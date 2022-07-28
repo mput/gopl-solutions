9933fca49b712985df5197713f74e5bdafa73649
mput <putintsev@gmail.com>
2022-07-28 15:18:02 +0200

    (07.18) xml parser - add stack solution.

--- ex-07-018/ex-07-018.go
+++ ex-07-018/ex-07-018.go
@@ -41,6 +41,8 @@ func (e *Element) String() string  {
 	return elString(e, 0)
 }
 
+// RECURSION SOLUTION
+
 func BuildNode(d *xml.Decoder, root *Element) (*Element, error) {
 	for {
 		token, err := d.Token()
@@ -82,24 +84,106 @@ func Parse(r io.Reader) (*Element, error) {
 		return rootNode, err
 	}
 
-	for _, v := range rootNode.Children {
+	return extractFirstElmnt(rootNode)
+}
+
+func extractFirstElmnt (e *Element) (*Element, error) {
+	for _, v := range e.Children {
 		v, ok := v.(*Element)
 		if ok {
 			return v, nil
 		}
 	}
-
 	return &Element{}, fmt.Errorf("Miss any nodes")
+}
+
+// STACK SOLUTION
+
+func ParseStack(r io.Reader) (*Element, error) {
+	d := xml.NewDecoder(r)
+	st := make(elmStack, 0)
+	st.push(&Element{
+		Type: xml.Name{Local: "root-service-element"},
+	})
+
+	for {
+		token, err := d.Token()
+		if err == io.EOF {
+			if rootElm, ok := st.pop(); ok {
+				return extractFirstElmnt(rootElm)
+			}
+			return &Element{}, fmt.Errorf("wrong xml document structure")
+		}
+		if err != nil {
+			return &Element{}, err
+		}
+
+		switch token := token.(type) {
+		case xml.StartElement:
+			nextNodePtr := &Element{
+				Type: token.Name,
+				Attr: token.Attr,
+			}
+			st.addChild(nextNodePtr)
+			st.push(nextNodePtr)
+		case xml.CharData:
+			s := strings.TrimSpace(string(token))
+			if s != "" {
+				st.addChild(s)
+			}
+		case xml.EndElement:
+			st.pop()
+		}
+	}
+}
+
+type elmStack []*Element
+
+func (st *elmStack) push (el *Element) {
+	*st = append(*st, el)
+}
+
+func (st *elmStack) pop () (*Element, bool) {
+	l := len(*st)
+	if l < 1 {
+		return &Element{}, false
+	}
+	elp := (*st)[l-1]
+	*st = (*st)[:l-1]
+	return elp, true
+}
+
+func (st *elmStack) addChild (el Node) {
+	l := len(*st)
+	(*st)[l-1].Children = append((*st)[l-1].Children, el)
 }
 
 func main() {
 	// res, err := Parse(os.Stdin)
-	res, err := Parse(strings.NewReader(blob))
+	var in io.Reader
+	in = strings.NewReader(blob)
+	// in = os.Stdin
+	resReq, err := Parse(in)
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
+		fmt.Printf("Error Req Solution: %s\n", err)
 		os.Exit(1)
 	}
-	fmt.Println(res.String())
+	fmt.Printf(
+		"Recursion solution: \n%s\n------------------\n",
+		resReq,
+	)
+
+	in = strings.NewReader(blob)
+
+	resStack, err := ParseStack(in)
+	if err != nil {
+		fmt.Printf("Error Stack Solution: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf(
+		"Stack solution: \n%s\n------------------\n",
+		resStack,
+	)
 }
 
 
